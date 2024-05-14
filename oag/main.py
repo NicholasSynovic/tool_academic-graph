997aa541601f42fa55918b87241748894f4e2b5a
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-14 11:46:10 -0500

    Format columns to specific date types

--- oag/main.py
+++ oag/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import List
 
 import click
+import pandas
 from pandas import DataFrame
 from pyfs import isFile, resolvePath
 from sqlalchemy import Engine
@@ -13,7 +14,18 @@ from oag.sqlite.db import DB
 
 
 def insertWorks(df: DataFrame, dbConn: Engine) -> None:
-    saveData(df=df, table="works", dbConn=dbConn)
+    df["oa_id"] = df["oa_id"].str.replace(pat="https://openalex.org/", repl="")
+    df["doi"] = df["doi"].str.replace(pat="https://doi.org/", repl="")
+    df["title"] = df["title"].str.title()
+    df["paratext"] = df["paratext"].astype(dtype=bool)
+    df["retracted"] = df["retracted"].astype(dtype=bool)
+    df["published"] = pandas.to_datetime(
+        arg=df["published"],
+        errors="coerce",
+        format="%Y-%m-%d",
+    )
+    foo: DataFrame = df.dropna(subset=["published"])
+    saveData(df=foo, table="works", dbConn=dbConn)
 
 
 @click.command()
