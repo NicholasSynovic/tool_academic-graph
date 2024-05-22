f7ef326c1ae3e89850ee282a112ec5a96a12fa0e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-22 08:12:41 -0500

    Restore package name to main

--- graphML/go.mod
+++ graphML/go.mod
@@ -1,3 +1,5 @@
 module NicholasSynovic/graphml-generator
 
 go 1.22.3
+
+require github.com/mattn/go-sqlite3 v1.14.22
--- graphML/main.go
+++ graphML/main.go
@@ -1,1 +1,25 @@
-package graphml
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func connectToDatabase(fp string) *sql.DB {
+	db, err := sql.Open("sqlite3", fp)
+
+	if err != nil {
+		fmt.Println("Error connecting to:", filepath.Base(fp))
+		os.Exit(1)
+	}
+
+	return db
+}
+
+func main() {
+	connectToDatabase("../test2.db")
+}
