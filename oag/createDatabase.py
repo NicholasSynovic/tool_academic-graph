671410205fad3de16c82e1bbbdcc617f12d16419
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-05-21 21:46:06 -0500

    Update code to add cites to database

--- oag/createDatabase.py
+++ oag/createDatabase.py
@@ -10,14 +10,39 @@ from oag.sqlite import createDBConnection
 from oag.sqlite.db import DB
 
 
+def readWrite(
+    fp: Path,
+    dbPath: Path,
+    columns: dict[str, str],
+    dbConn: Engine,
+    dbTable: str,
+    index: bool = False,
+) -> None:
+    print(f"Reading: ", fp.name)
+    df: DataFrame = pandas.read_json(path_or_buf=fp)
+    df.rename(columns=columns, inplace=True)
+    df.columns = df.columns.str.lower()
+
+    print(f"Writing to: ", dbPath.name)
+    df.to_sql(name=dbTable, con=dbConn, if_exists="append", index=index)
+
+
 @click.command()
 @click.option(
-    "-i",
-    "--input",
-    "inputFP",
+    "-w",
+    "--input-works-file",
+    "worksFP",
     type=Path,
     required=True,
-    help="Path to JSON file",
+    help="Path to Works JSON file",
+)
+@click.option(
+    "-c",
+    "--input-cites-file",
+    "citesFP",
+    type=Path,
+    required=True,
+    help="Path to Citations JSON file",
 )
 @click.option(
     "-o",
@@ -27,9 +52,11 @@ from oag.sqlite.db import DB
     required=True,
     help="Path to output database",
 )
-def main(inputFP: Path, outputFP: Path) -> None:
-    absInputFP: Path = resolvePath(path=inputFP)
-    assert isFile(absInputFP)
+def main(worksFP: Path, citesFP: Path, outputFP: Path) -> None:
+    absWorksFP: Path = resolvePath(path=worksFP)
+    absCitessFP: Path = resolvePath(path=citesFP)
+    assert isFile(absWorksFP)
+    assert isFile(absCitessFP)
 
     absOutputFP: Path = resolvePath(path=outputFP)
 
@@ -37,20 +64,33 @@ def main(inputFP: Path, outputFP: Path) -> None:
     db: DB = DB(dbConn=dbConn)
     del db
 
-    print(f"Reading: ", absInputFP.name)
-    df: DataFrame = pandas.read_json(path_or_buf=absInputFP)
-    df.rename(
-        columns={
-            "Is_Paratext": "paratext",
-            "Is_Retracted": "retracted",
-            "Date_Published": "published",
-        },
-        inplace=True,
-    )
-    df.columns = df.columns.str.lower()
+    worksColumns: dict[str, str] = {
+        "Is_Paratext": "paratext",
+        "Is_Retracted": "retracted",
+        "Date_Published": "published",
+    }
 
-    print(f"Writing to: ", absOutputFP.name)
-    df.to_sql(name="works", con=dbConn, if_exists="append", index=False)
+    citesColumns: dict[str, str] = {
+        "Work_OA_ID": "work",
+        "Ref_OA_ID": "reference",
+    }
+
+    readWrite(
+        fp=absWorksFP,
+        dbPath=absOutputFP,
+        columns=worksColumns,
+        dbConn=dbConn,
+        dbTable="works",
+    )
+
+    readWrite(
+        fp=absCitessFP,
+        dbPath=absOutputFP,
+        columns=citesColumns,
+        dbConn=dbConn,
+        dbTable="cites",
+        index=True,
+    )
 
 
 if __name__ == "__main__":
