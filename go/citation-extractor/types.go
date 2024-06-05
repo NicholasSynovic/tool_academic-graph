b6723f3a06e0574c8f8015637cd346ee2034f47c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-05 13:34:24 -0500

    Add boilerplate code

---
+++ go/citation-extractor/types.go
@@ -0,0 +1,18 @@
+package main
+
+/*
+Type to represent application configuration
+
+Modified by user command line input
+*/
+type AppConfig struct {
+	inputPath, outputPath string
+}
+
+/*
+Type of a Citation
+*/
+type Citation struct {
+	SOURCE string `json:"source"`
+	DEST   string `json:"dest"`
+}
