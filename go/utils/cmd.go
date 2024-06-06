a1091435b610b7282f5f72338301adfa40004b3f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-06 09:10:15 -0500

    Fix errors

--- go/utils/cmd.go
+++ go/utils/cmd.go
@@ -15,32 +15,38 @@ storeObject string:	A string that is uesed for the "-o" flag to add to the help 
 returns AppConfig:	An AppConfig struct of the "-i" and "-o" inputs
 */
 func ParseCommandLine(storeObject string) AppConfig {
-	config := AppConfig{inputPath: "oa_works.json", outputPath: "output.json"}
+	config := AppConfig{InputPath: "oa_works.json", OutputPath: "output.json"}
 
-	flag.StringVar(&config.inputPath, "i", config.inputPath, `Path to OpenAlex "Works" JSON Lines file`)
+	flag.StringVar(&config.InputPath, "i", config.InputPath, `Path to OpenAlex "Works" JSON Lines file`)
 
-	flag.StringVar(&config.outputPath, "o", config.outputPath, `Path to output JSON file to store `+storeObject)
+	flag.StringVar(&config.OutputPath, "o", config.OutputPath, `Path to output JSON file to store `+storeObject)
 
 	flag.Parse()
 
-	config.inputPath, _ = filepath.Abs(config.inputPath)
-	config.outputPath, _ = filepath.Abs(config.outputPath)
+	config.InputPath, _ = filepath.Abs(config.InputPath)
+	config.OutputPath, _ = filepath.Abs(config.OutputPath)
 
 	testValidInputs(config)
 
 	return config
 }
 
+/*
+Ensure that AppConfig values:
+
+	exist in the file system (InputPath),
+	do not exist in the file system (OutputPath)
+*/
 func testValidInputs(config AppConfig) {
-	_, err := os.Stat(config.inputPath)
+	_, err := os.Stat(config.InputPath)
 	if err != nil {
-		fmt.Println("ERROR: input doesn't exist:", config.inputPath)
+		fmt.Println("ERROR: input doesn't exist:", config.InputPath)
 		os.Exit(1)
 	}
 
-	_, err = os.Stat(config.outputPath)
+	_, err = os.Stat(config.OutputPath)
 	if err == nil {
-		fmt.Println("ERROR: output exist:", config.outputPath)
+		fmt.Println("ERROR: output exist:", config.OutputPath)
 		os.Exit(1)
 	}
 }
