6182d2b91f78dcc166eb2e181f982466294be9b8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-06 09:11:16 -0500

    Add document type

--- go/utils/types.go
+++ go/utils/types.go
@@ -1,5 +1,7 @@
 package utils
 
+import "time"
+
 /*
 Type to represent application configuration
 
@@ -10,9 +12,23 @@ type AppConfig struct {
 }
 
 /*
-Type of a Citation
+Citation Relationship type
 */
 type Citation struct {
 	SOURCE string `json:"source"`
 	DEST   string `json:"dest"`
+}
+
+/*
+Document type
+*/
+type Document struct {
+	DOI              string    `json:"doi"`
+	TITLE            string    `json:"title"`
+	PUBLICATION_DATE time.Time `json:"publication_date"`
+	OA_TYPE          string    `json:"openalex_type"`
+	CR_TYPE          string    `json:"crossref_type"`
+	CITED_BY_COUNT   int       `json:"cited_by_count"`
+	RETRACTED        bool      `json:"is_retracted"`
+	OPEN_ACCESS      bool      `json:"is_open_access"`
 }
