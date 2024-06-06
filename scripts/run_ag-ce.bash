6c86179b203c8b17d28429f8ef17c9e42dadecee
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-06 15:20:32 -0500

    Create script to extract citation information from OA Works files

---
+++ scripts/run_ag-ce.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+source optparse.bash
+optparse.define short=i long=input desc="Directory containing OpenAlex \"Works\" JSON documents" variable=inputPath
+source $( optparse.build )
+
+absProgPath=$(readlink -f ../go/bin)
+absInputPath=$(readlink -f $inputPath)
+
+if [ ! -d $absInputPath ]; then
+    echo "$absInputPath is a file. Expected a directory."
+fi
+
+cd ../go/bin
+mkdir -p data
+
+ls $absInputPath/part*.json | xargs -I % basename % | xargs -I {} $absProgPath/ag-ce.bin -i $absInputPath/{} -o data/ce_{}.json
