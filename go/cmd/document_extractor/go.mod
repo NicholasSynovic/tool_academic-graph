dc955cc35a4b60d62e3242eef86278cb031132c1
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-06 09:12:21 -0500

    Start work on document extractor code

---
+++ go/cmd/document_extractor/go.mod
@@ -0,0 +1,3 @@
+module ag/document_extractor
+
+go 1.22.3
---
+++ go/cmd/document_extractor/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"ag/utils"
+
+	"github.com/schollz/progressbar/v3"
+)
+
+func main() {
+	var jsonObjs []map[string]any
+
+	config := utils.ParseCommandLine("citation relationships")
+
+	inputFP := utils.OpenFile(config.InputPath)
+	defer inputFP.Close()
+
+	jsonLinesStringChan := make(chan string)
+	jsonObjsChan := make(chan map[string]any)
+
+	go utils.ReadLines(inputFP, jsonLinesStringChan)
+	go utils.CreateJSONObjs(jsonLinesStringChan, jsonObjsChan)
+
+	bar := progressbar.Default(-1, "Collecting JSON objects...")
+	for data := range jsonObjsChan {
+		jsonObjs = append(jsonObjs, data)
+		bar.Add(1)
+	}
+	bar.Exit()
+
+}
