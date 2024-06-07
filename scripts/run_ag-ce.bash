75b27af2391a88503b9255db37d4596b5a1c9abc
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-07 14:51:32 -0500

    Rename suffix variable

--- scripts/run_ag-ce.bash
+++ scripts/run_ag-ce.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 source optparse.bash
 optparse.define short=i long=input desc="Directory containing OpenAlex \"Works\" JSON documents" variable=inputPath
-optparse.define short=s long=suffix desc="Suffix for output JSON files" variable=suffix
+optparse.define short=s long=suffix desc="Suffix for output JSON files" variable=fileSuffix
 source $( optparse.build )
 
 absProgPath=$(readlink -f ../go/bin)
@@ -14,4 +14,4 @@ fi
 cd ../go/bin
 mkdir -p data
 
-ls $absInputPath/part* | xargs -I % basename % | xargs -I % $absProgPath/ag-ce.bin -i $absInputPath/% -o data/ce_$suffix_%.json
+ls $absInputPath/part* | xargs -I % basename % | xargs -I % $absProgPath/ag-ce.bin -i $absInputPath/% -o data/ce_$fileSuffix_%.json
