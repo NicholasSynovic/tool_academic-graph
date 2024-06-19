89c620eea7c19639f796b7857f3364a9c4518430
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-19 11:11:36 -0500

    Create authorship relationship uploader file

---
+++ python/src/authorshipRelationship.py
@@ -0,0 +1,76 @@
+from math import ceil
+from pathlib import Path
+from typing import List
+
+import click
+import pandas
+from pandas import DataFrame
+from pandas.io.json._json import JsonReader
+from progress.bar import Bar
+from pyfs import isFile, resolvePath
+from sqlalchemy import Engine
+from src import createDBConnection, saveData
+from src.db import DB
+from src.shell import getJSONSize
+
+
+@click.command()
+@click.option(
+    "-i",
+    "--input",
+    "inputPath",
+    type=Path,
+    help="Path to OpenAlex JSON Authorship Relationship File generated from Go scripts",
+    required=True,
+)
+@click.option(
+    "-o",
+    "--output",
+    "outputPath",
+    type=Path,
+    help="Path to SQLite3 Database to store JSON Authorship Relationship data",
+    required=True,
+)
+@click.option(
+    "--chunksize",
+    "chunksize",
+    type=int,
+    help="Chunksize while iterating through JSON files",
+    required=False,
+    default=10000,
+)
+def main(inputPath: Path, outputPath: Path, chunksize: int) -> None:
+    absInputPath: Path = resolvePath(path=inputPath)
+    absOutputPath: Path = resolvePath(path=outputPath)
+
+    assert isFile(path=absInputPath)
+
+    engine: Engine = createDBConnection(dbPath=absOutputPath)
+    db: DB = DB(dbConn=engine)
+
+    print("Getting the total number of objects in the JSON file...")
+    objCount: int = getJSONSize(fp=absInputPath)
+
+    dfs: JsonReader[DataFrame] = pandas.read_json(
+        path_or_buf=absInputPath,
+        lines=True,
+        chunksize=chunksize,
+        compression="infer",
+    )
+
+    with Bar(
+        "Iterating through DataFrames...", max=int(ceil(objCount / chunksize))
+    ) as bar:
+        for df in dfs:
+            df.set_index(keys="id", inplace=True)
+            saveData(
+                df=df,
+                table="relationship_authorship",
+                dbConn=engine,
+                includeIndex=True,
+            )
+            bar.next()
+
+
+if __name__ == "__main__":
+    main()
