4b13e8b6b30018b46c515113b832d73845fbc67b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-19 20:45:21 -0500

    Delete scripts

--- scripts/createCompressedNeo4JDump.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source optparse.bash
-optparse.define short=i long=input desc="The Neo4J database to dump" variable=db default="neo4j"
-source $( optparse.build )
-
-sudo neo4j-admin database dump $db --to-path .
-
-tar -czvf $db.dump.tar.gz $db.dump
